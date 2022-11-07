459f3fded654a778fe5b720b0a1763cb02061205
muac404 <muac404@gmail.com>
2022-11-07 01:25:42 +0100

    genera numeri casuali

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
 "use strict"
+
+
+// FUNCTIONS
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+
+// MAIN
+
+
+
+const numberArray = []
+
+let i = 0
+
+while (numberArray.length < 5) {
+    const memoryNumber = getRndInteger(1, 100);
+    if( !numberArray.includes(memoryNumber)){
+        numberArray.push(memoryNumber); 
+    }
+    
+    
+    i++; 
+}
+
+console.log(numberArray)
+const container = document.getElementById("container")
+
+const boardNumber = document.createElement("div");
+boardNumber.innerHTML = numberArray
+container.append(boardNumber)
